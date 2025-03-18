0ea201438fbb4e16d858db1e37e3e38ed7485d15
Peter Salvi <salvi@iit.bme.hu>
2025-03-18 10:15:46 +0100

    Fuzzy classification

--- classifier.cc
+++ classifier.cc
@@ -16,11 +16,15 @@ Quadric::Type Quadric::classify(double tolerance) const {
   Vector3d eigenvalues = eigensolver.eigenvalues();
   Matrix3d eigenvectors = eigensolver.eigenvectors();
 
+  auto fuzzy = [=](double x) {
+    return std::abs(x) <= tolerance ? 0.0 : x;
+  };
+
   // Count positive, negative, and zero eigenvalues and save non-zero indices
   int positiveCount = 0, negativeCount = 0, zeroCount = 0;
   std::vector<int> indices;
   for (int i = 0; i < 3; ++i) {
-    if (std::abs(eigenvalues[i]) <= tolerance)
+    if (fuzzy(eigenvalues[i]) == 0)
       ++zeroCount;
     else {
       indices.push_back(i);
@@ -44,13 +48,13 @@ Quadric::Type Quadric::classify(double tolerance) const {
     else
       w0 << 0, w2[2], -w2[1];
     Vector3d w1 = w2.cross(w0);
-    auto d0 = w0.dot(B);
-    auto d1 = w1.dot(B);
+    auto d0 = fuzzy(w0.dot(B));
+    auto d1 = fuzzy(w1.dot(B));
     if (d0 != 0 || d1 != 0)
       return PARABOLIC_CYLINDER;
     auto e2 = w2.dot(A * w2);
     auto d2 = w2.dot(B);
-    auto r = d2 * d2 / (4 * e2) - C;
+    auto r = fuzzy(d2 * d2 / (4 * e2) - C);
     if (positiveCount == 1)
       return r > 0 ? TWO_PLANES : (r < 0 ? NO_SURFACE : PLANE);
     return r < 0 ? TWO_PLANES : (r > 0 ? NO_SURFACE : PLANE);
@@ -61,13 +65,13 @@ Quadric::Type Quadric::classify(double tolerance) const {
     Vector3d w2 = eigenvectors.col(indices[1]);
     Vector3d w0 = w1.cross(w2);
     w2 = w0.cross(w1);
-    auto d0 = w0.dot(B);
+    auto d0 = fuzzy(w0.dot(B));
     if (d0 != 0)
       return positiveCount == negativeCount ? HYPERBOLIC_PARABOLOID : ELLIPTIC_PARABOLOID;
     Vector3d Aw1 = A * w1, Aw2 = A * w2;
     Matrix2d E; E << w1.dot(Aw1), w1.dot(Aw2), w1.dot(Aw2), w2.dot(Aw2);
     Vector2d f; f << w1.dot(B), w2.dot(B);
-    auto r = f.dot(E.inverse() * f) / 4 - C;
+    auto r = fuzzy(f.dot(E.inverse() * f) / 4 - C);
     if (positiveCount == 2)
       return r > 0 ? ELLIPTIC_CYLINDER : NO_SURFACE;
     if (negativeCount == 2)
@@ -76,7 +80,7 @@ Quadric::Type Quadric::classify(double tolerance) const {
   }
 
   // Full rank
-  double r = B.dot(A.inverse() * B) / 4 - C;
+  double r = fuzzy(B.dot(A.inverse() * B) / 4 - C);
   if (positiveCount == 3)
     return r > 0 ? ELLIPSOID : NO_SURFACE;
   if (positiveCount == 2)
